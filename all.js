7cb78fba3c09d174e4bfef14f288990a8b329e6f
weiweihuang93 <weiwei.huang.93@gmail.com>
2024-05-22 08:47:58 +0800

    update

--- all.js
+++ all.js
@@ -1,19 +1,11 @@
-// $('.hamburger .btn').click(function () {
-//     $('.header-sm-nav').toggleClass('open');
-//     $('.header-sm-content').toggleClass('open');
-//     $('.hamburger .btn').toggleClass('active').toggleClass('close');
-//   });
-
 $(document).ready(function(){
 
-    $('.btn').click(function(){
+    $('.navbar-btn').click(function(){
 
-        $('.btn').toggleClass('active');
-        $('.hamburger .open').toggleClass('close');
-        $('.closure').toggleClass('close');
-        $(".header-group-sm").toggleClass("close");
-
-        $(".header-group-sm").toggle();
+        $('.navbar-btn').toggleClass('active');
+        $('.btn').toggleClass('close');
+        
+        $(".header-group").toggleClass("active");
     })
 
 });
